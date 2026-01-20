9dc4f17528bd6ed4a67642db101d29b33fd14252
codychun <codyc2023.red@gmail.com>
2026-01-20 13:15:47 +0000

    London Photo Galleries

--- src/data/galleries.js
+++ src/data/galleries.js
@@ -44,9 +44,74 @@ export const galleries = {
   },
   campus: {
     name: 'London',
-    description: 'Spring 2026 abroad in London',
+    description: 'Spring 2026 abroad in London, UK',
     photos: [
-      // Add photos when available in /photos/Campus/
+      {
+        id: 1,
+        url: '/photos/london/nelson_column.JPG',
+        alt: 'Nelson\'s Column',
+        caption: 'Nelson\'s Column in Trafalgar Square on the way to class.'
+      },
+      {
+        id: 2,
+        url: '/photos/london/bigben_1.JPG',
+        alt: 'Big Ben Dramatic',
+        caption: 'Big Ben from the National Gallery in Trafalgar Square on the way to class.'
+      },
+      {
+        id: 3,
+        url: '/photos/london/bigben_2.JPG',
+        alt: 'Big Ben Cinematic',
+        caption: 'Another shot of Big Ben from Trafalgar Square.'
+      },
+      {
+        id: 4,
+        url: '/photos/london/evan_cathedral.JPG',
+        alt: 'Evan and Southwark Cathedral',
+        caption: 'Evan and Southwark Cathedral on the Notre Dame Big Bus Tour.'
+      },
+      {
+        id: 5,
+        url: '/photos/london/figaro.JPG',
+        alt: 'Pink Nissan Figaro',
+        caption: 'Pink car parked on the street. Identified a Nissan Figaro by Landon.'
+      },
+      {
+        id: 6,
+        url: '/photos/london/evan_radios.JPG',
+        alt: 'Evan and Radios',
+        caption: 'Evan and radios in the Tate Modern.'
+      },
+      {
+        id: 7,
+        url: '/photos/london/radios.JPG',
+        alt: 'Radios',
+        caption: 'Tower of radios in the Tate Modern.'
+      },
+      {
+        id: 8,
+        url: '/photos/london/yellow_stairs.JPG',
+        alt: 'Yellow Stairs',
+        caption: 'Yellow stairs lead where?'
+      },
+      {
+        id: 9,
+        url: '/photos/london/trafalgar_crowd.JPG',
+        alt: 'Trafalgar Crowd',
+        caption: 'Crowded Trafalgar Square in front of National Gallery.'
+      },
+      {
+        id: 10,
+        url: '/photos/london/rbo_hall.JPG',
+        alt: 'Royal Ballet and Opera',
+        caption: 'Royal Ballet and Opera Hall.'
+      },
+      {
+        id: 11,
+        url: '/photos/london/george4.JPG',
+        alt: 'George IV Equestrian',
+        caption: 'George IV Equestrian statue in Trafalgar Square.'
+      }
     ]
   }
 }
